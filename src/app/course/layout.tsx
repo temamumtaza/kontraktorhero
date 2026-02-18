71cf45dc47f0907c3bd1f11c08489364e9955a75
tememumtaza <reizants17@gmail.com>
2026-02-18 09:51:57 +0700

    fix: change AuthGuard to default import

--- src/app/course/layout.tsx
+++ src/app/course/layout.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
-import { AuthGuard } from "@/components/AuthGuard";
+import AuthGuard from "@/components/AuthGuard";
 import {
     Brain,
     Building2,
